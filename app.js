5c07d6686de0eda959404eb86358cb8d7de15b80
passionzjane <mrumunjanet@gmail.com>
2022-11-05 06:41:20 +0100

    created an operation  using add, sub, and multi

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT
 
 const app = express()
+app.use(express.json())
 
 
 app.get('/', async (req, res) => {
@@ -16,6 +17,49 @@ app.get('/', async (req, res) => {
 
     }
     res.status(200).json(person)
+})
+
+
+app.post('/', async (req, res) => {
+    const Enum = {
+        addition: "addition",
+        subtraction: "subtraction",
+        multiplication: "multiplication"
+    }
+    
+    
+    var {operation_type, x, y } = req.body;
+    operation_type = operation_type.toLowerCase();
+
+    if(operation_type === 'addition') {
+
+        return res.status(200)
+        .json({
+            slackUsername: "janeStack",
+            result: x + y,
+            operation_type: Enum.addition
+        })
+    }
+
+    if(operation_type === 'subtraction') {
+
+        return res.status(200)
+        .json({
+            slackUsername: "janeStack",
+            result: x - y,
+            operation_type: Enum.subtraction
+        })
+    }
+
+    if(operation_type === 'multiplication') {
+
+        return res.status(200)
+        .json({
+            slackUsername: "janeStack",
+            result: x * y,
+            operation_type: Enum.multiplication
+        })
+    }
 })
 
 app.listen(port, () => console.log(`App is running on port ${port}`))
